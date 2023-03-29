a34b91edb0ad851c3a26fb1f471bfefd39d7dad2
Andrej Troshin <darkusss13@ya.ru>
2023-03-29 09:06:47 +0300

    4commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-from .permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticated 
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet 
+from posts.models import Group, Post 
+from .permissions import IsAuthorOrReadOnly 
 from .serializers import GroupSerializer, PostSerializer, CommentSerializer
-from posts.models import Group, Post
 
 
 class GroupViewSet(ReadOnlyModelViewSet):
@@ -12,7 +12,7 @@ class GroupViewSet(ReadOnlyModelViewSet):
 
 
 class PostViewSet(ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.select_related('author').all()
     serializer_class = PostSerializer
     permission_classes = [
         IsAuthenticated,
@@ -23,17 +23,20 @@ class PostViewSet(ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class CommentViewSet(ModelViewSet):
-    serializer_class = CommentSerializer
-    permission_classes = [
-        IsAuthenticated,
-        IsAuthorOrReadOnly
-    ]
+class CommentViewSet(ModelViewSet): 
+    serializer_class = CommentSerializer 
+    permission_classes = [ 
+        IsAuthenticated, 
+        IsAuthorOrReadOnly 
+    ] 
 
-    def perform_create(self, serializer):
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        serializer.save(author=self.request.user, post=post)
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
 
-    def get_queryset(self):
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+    def perform_create(self, serializer): 
+        post = self.get_post() 
+        serializer.save(author=self.request.user, post=post) 
+
+    def get_queryset(self): 
+        post = self.get_post() 
         return post.comments
